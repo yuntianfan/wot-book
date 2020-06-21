23436e994f917322c1daa25fe485095e25d3e71c
yuntianfan <yuntianfan90@gmail.com>
2020-06-21 11:10:30 +0800

    reading a PIR sensor

--- chapter4-gpios/pir.js
+++ chapter4-gpios/pir.js
@@ -1,15 +1,20 @@
 var Gpio = require('onoff').Gpio,
-	sensor = new Gpio(17, 'in', 'both');
+	sensor = new Gpio(17, 'in', 'both'),
+	interval;
 	
 sensor.watch(function (err, value) {
 	if (err) exit(err);
 	console.log(value? 'there is some one!' : 'not anymore!');
 });
 
-function exit(err) {
-	if (err)	console.log('An error occured: ' + err);
+interval = setInterval(function () {
 	var value = sensor.readSync();
 	console.log('sensor value is ' + value);
+}, 2000);
+
+function exit(err) {
+	if (err)	console.log('An error occured: ' + err);
+	clearInterval(interval);
 	sensor.unexport();
 	console.log('Bye, bye!');
 	process.exit();
